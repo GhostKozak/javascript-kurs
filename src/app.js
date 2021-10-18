dc50f969baffb02bc8cd687a2d4f32e5e6fb27e2
Gökhan Tanrıverdi <codeodd3@gmail.com>
2021-10-19 00:02:51 +0300

    Start Project

---
+++ src/app.js
@@ -0,0 +1,7 @@
+let konutKredileri = ["KK","EKK","KKK", "ÖKK"]
+
+console.log("<ul>")
+for(let i = 0;i<konutKredileri.length;i++) {
+    console.log(`<li> ${konutKredileri[i]} </li>`)
+}
+console.log("</ul>")
